3cfaea32699d50a94bfb9f05c6226a447160a42f
mansourlotfi <mansourlotfi@gmail.com>
2020-06-01 22:50:32 +0430

    minor fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Faq from './view/faq';
 import Sample1 from './view/sample1';
+import Sample2 from './view/sample2';
 export default function App() {
 	return (
 		<Router>
@@ -16,13 +17,15 @@ export default function App() {
 							<Link to="/sample1">api call using hooks</Link>
 						</li>
 						<li>
-							<Link to="/sample2">grid data api call</Link>
+							<Link to="/sample2">sample2</Link>
 						</li>
 					</ul>
 				</nav>
 
 				<Switch>
-					<Route path="/sample2">{/* <Sample1 /> */}</Route>
+					<Route path="/sample2">
+						<Sample2 />
+					</Route>
 					<Route path="/sample1">
 						<Sample1 />
 					</Route>
---
+++ src/view/sample2/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Sample2(props) {
+	return (
+		<div style={{ textAlign: 'center' }}>
+			test
+			<div>asd</div>
+		</div>
+	);
+}
+
+export default Sample2;
